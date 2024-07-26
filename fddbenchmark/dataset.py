6aced5a5c9abeba9bbe0a962aa66388a4e2ff38f
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-07-26 20:25:34 +0300

    [refactor] FDDDataset class

--- fddbenchmark/dataset.py
+++ fddbenchmark/dataset.py
@@ -1,31 +1,30 @@
 import os
+from typing import List
+
 from tqdm.auto import tqdm
+from fddbenchmark.config import AVAILABLE_DATASETS, SERVER_URL, DATA_FOLDER
 import zipfile
 import requests
 import pandas as pd
 
+
 class FDDDataset():
     def __init__(self, name: str):
         self.name = name
-        self.df = None
-        self.label = None
-        self.train_mask = None
-        self.test_mask = None
-        available_datasets = ['small_tep', 'reinartz_tep', 'rieth_tep', 'lessmeier_bearing']
-        available_datasets_str = ', '.join(available_datasets)
-        if self.name not in available_datasets:
+        available_datasets_str = ', '.join(AVAILABLE_DATASETS)
+        if self.name not in AVAILABLE_DATASETS:
             raise Exception(
                 f'{name} is an unknown dataset. Available datasets are: {available_datasets_str}'
             )
 
-        ref_path = f'data/{self.name}/'
+        ref_path = f'{DATA_FOLDER}/{self.name}/'
         if not os.path.exists(ref_path):
             os.makedirs(ref_path)
-        url = f'https://industrial-makarov.obs.ru-moscow-1.hc.sbercloud.ru/{self.name}.zip'
-        zfile_path = f'data/{self.name}.zip'
+        url = f'{SERVER_URL}/{self.name}.zip'
+        zfile_path = f'{DATA_FOLDER}/{self.name}.zip'
         if not os.path.exists(zfile_path):
             download_pgbar(url, zfile_path, fname=f'{self.name}.zip')
-        
+
         extracting_files(zfile_path, ref_path)
         self.df = read_csv_pgbar(ref_path + 'dataset.csv', index_col=['run_id', 'sample'])
         self.label = read_csv_pgbar(ref_path + 'labels.csv', index_col=['run_id', 'sample'])['labels']
@@ -35,7 +34,7 @@ class FDDDataset():
         self.test_mask = test_mask.astype('boolean')
 
 
-def extracting_files(zfile_path, ref_path, bsize=1024*10000):
+def extracting_files(zfile_path: str, ref_path: str, bsize: int = 1024 * 10000) -> None:
     with zipfile.ZipFile(zfile_path, 'r') as zfile:
         for entry_info in zfile.infolist():
             if os.path.exists(ref_path + entry_info.filename):
@@ -44,11 +43,11 @@ def extracting_files(zfile_path, ref_path, bsize=1024*10000):
             target_file = open(ref_path + entry_info.filename, 'wb')
             block = input_file.read(bsize)
             with tqdm(
-                total=entry_info.file_size, 
-                desc=f'Extracting {entry_info.filename}', 
-                unit='B', 
-                unit_scale=True, 
-                unit_divisor=1024) as pbar:
+                    total=entry_info.file_size,
+                    desc=f'Extracting {entry_info.filename}',
+                    unit='B',
+                    unit_scale=True,
+                    unit_divisor=1024) as pbar:
                 while block:
                     target_file.write(block)
                     block = input_file.read(bsize)
@@ -57,22 +56,22 @@ def extracting_files(zfile_path, ref_path, bsize=1024*10000):
             target_file.close()
 
 
-def download_pgbar(url, zfile_path, fname, chunk_size=1024):
+def download_pgbar(url: str, zfile_path: str, fname: str, chunk_size: int = 1024) -> None:
     resp = requests.get(url, stream=True)
     total = int(resp.headers.get("Content-Length"))
-    with open(zfile_path, 'wb') as file: 
+    with open(zfile_path, 'wb') as file:
         with tqdm(
-            total=total,
-            desc=f'Downloading {fname}',
-            unit='B',
-            unit_scale=True,
-            unit_divisor=1024) as pbar:
+                total=total,
+                desc=f'Downloading {fname}',
+                unit='B',
+                unit_scale=True,
+                unit_divisor=1024) as pbar:
             for data in resp.iter_content(chunk_size=chunk_size):
                 file.write(data)
                 pbar.update(len(data))
 
 
-def read_csv_pgbar(csv_path, index_col, chunksize=1024*100):
+def read_csv_pgbar(csv_path: str, index_col: List[str], chunksize: int = 1024 * 100) -> pd.DataFrame:
     rows = sum(1 for _ in open(csv_path, 'r')) - 1
     chunk_list = []
     with tqdm(total=rows, desc=f'Reading {csv_path}') as pbar:
