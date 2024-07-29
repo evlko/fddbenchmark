8b4363823cf9ffcad23a42b0ad3ed2707973bfb0
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-07-29 21:09:21 +0300

    [add] FDDDataLoaderDask

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -1,25 +1,26 @@
 import numpy as np
 
 from fddbenchmark import FDDDataset
-from fddbenchmark.dataloader import FDDDataloaderPandas
+from fddbenchmark.dataloader import FDDDataLoaderDask
 
 
 def test_small_tep():
     dataset = FDDDataset(name="small_tep")
-    loader = FDDDataloaderPandas(
+    loader = FDDDataLoaderDask(
         dataset=dataset,
         train=True,
         window_size=100,
         step_size=1,
         use_minibatches=True,
         batch_size=1024,
+        shuffle=False,
     )
     assert len(loader) == 42
     for ts, time_index, label in loader:
         break
-    assert ts.shape == (1024, 100, 0)
+    assert ts.shape == (1024, 100, 52)
     assert len(time_index) == 1024
     assert label.shape == (1024,)
-    # assert np.all(
-    #    ts[0, 0, :5] == [2.5038e-01, 3.6740e03, 4.5290e03, 9.2320e00, 2.6889e01]
-    # )
+    assert np.all(
+        ts[0, 0, :5] == [2.5038e-01, 3.6740e03, 4.5290e03, 9.2320e00, 2.6889e01]
+    )
