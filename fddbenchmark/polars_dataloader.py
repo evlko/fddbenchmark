690296b1b12f856db9fb82a8001ea964faad0e65
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-07-30 14:18:54 +0300

    [fix] code style polars

--- fddbenchmark/polars_dataloader.py
+++ fddbenchmark/polars_dataloader.py
@@ -1,27 +1,27 @@
 import sys
 from typing import Optional
 
-from fddbenchmark import FDDDataloader, FDDDataset
 import numpy as np
 import pandas as pd
 import polars as pl
 
+from fddbenchmark import FDDDataloader, FDDDataset
 from fddbenchmark.config import DATA_FOLDER
 from utils.time_tracker import time_tracker
 
 
 class PolarsDataloader(FDDDataloader):
     def __init__(
-            self,
-            dataset: FDDDataset,
-            train: bool,
-            window_size: int,
-            dilation: int = 1,
-            step_size: int = 1,
-            use_minibatches: bool = False,
-            batch_size: Optional[int] = None,
-            shuffle: bool = False,
-            random_state: Optional[int] = None,
+        self,
+        dataset: FDDDataset,
+        train: bool,
+        window_size: int,
+        dilation: int = 1,
+        step_size: int = 1,
+        use_minibatches: bool = False,
+        batch_size: Optional[int] = None,
+        shuffle: bool = False,
+        random_state: Optional[int] = None,
     ) -> None:
         super().__init__(
             dataset=dataset,
@@ -39,16 +39,11 @@ class PolarsDataloader(FDDDataloader):
         self.df = self.df.with_row_count("row_idx")
         print(sys.getsizeof(self.df))
 
-
     @time_tracker(return_time=False)
-    def get_batch(self,  indicies: np.ndarray) -> np.array:
-        all_indices = np.concatenate(indicies)
+    def get_batch(self, indices: np.ndarray) -> np.array:
+        all_indices = np.concatenate(indices)
         filtered_df = self.df.filter(pl.col("row_idx").is_in(all_indices))
-
         collected_df = filtered_df.collect()
-
         df_np = collected_df.to_numpy()
-
-        results = [df_np[idx] for idx in indicies]
+        results = [df_np[idx] for idx in indices]
         return results
-
