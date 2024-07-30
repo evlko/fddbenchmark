c79b6019f444f6e7b78fa3e329ff368896ebdf91
Piromant <puromant@gmail.com>
2024-07-30 21:34:43 +0300

    Added vaex dataloader

--- fddbenchmark/vaex_dataloader.py
+++ fddbenchmark/vaex_dataloader.py
@@ -7,18 +7,18 @@ from fddbenchmark import FDDDataloader, FDDDataset
 from utils.time_tracker import time_tracker
 
 
-class FDDPolarsDataloader(FDDDataloader):
+class FDDDVaexDataloader(FDDDataloader):
     def __init__(
-        self,
-        dataset: FDDDataset,
-        train: bool,
-        window_size: int,
-        dilation: int = 1,
-        step_size: int = 1,
-        use_minibatches: bool = False,
-        batch_size: Optional[int] = None,
-        shuffle: bool = False,
-        random_state: Optional[int] = None,
+            self,
+            dataset: FDDDataset,
+            train: bool,
+            window_size: int,
+            dilation: int = 1,
+            step_size: int = 1,
+            use_minibatches: bool = False,
+            batch_size: Optional[int] = None,
+            shuffle: bool = False,
+            random_state: Optional[int] = None,
     ) -> None:
         super().__init__(
             dataset=dataset,
@@ -32,15 +32,18 @@ class FDDPolarsDataloader(FDDDataloader):
             random_state=random_state,
         )
         self.df = vaex.open(self.path)
-        self.df = self.df.add_column('row_idx', np.arange(len(self.df)))
+        self.df["row_idx"] = vaex.vrange(0, len(self.df))
         print(sys.getsizeof(self.df))
 
     @time_tracker(return_time=False)
     def get_batch(self, indices: np.ndarray) -> np.ndarray:
         all_indices = np.concatenate(indices)
         unique_indices = np.unique(all_indices)
-        filtered_df = self.df[self.df.row_idx.isin(unique_indices)]
-        df_np = filtered_df.to_numpy(columns=filtered_df.get_column_names()[3:])
+        mask = self.df.row_idx.isin(unique_indices)
+        filtered_df = self.df[mask]
+        df_pd = filtered_df.to_pandas_df()
+        df_pd = df_pd.iloc[:, 2:-1]
+        df_np = df_pd.to_numpy()
         idx_map = {idx: i for i, idx in enumerate(unique_indices)}
         results = np.array([df_np[idx_map[idx]] for idx in all_indices])
         results = results.reshape(indices.shape + (-1,))
